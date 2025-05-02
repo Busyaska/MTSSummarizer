41aebc341f7bc9429213b3509e7f6d6bf87a1938
Busyaska <fox666g@gmail.com>
2025-05-02 11:08:20 +0500

    create model and serializers

--- backend/MTSSummarizerBackend/api/models.py
+++ backend/MTSSummarizerBackend/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Article(models.Model):
+    url = models.URLField('Ссылка на статью', max_length=128)
+    title = models.CharField('Название статьи', max_length=128)
+    summary = models.TextField('Пересказ статьи')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='articles', verbose_name='Пользователь')
+    created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
---
+++ backend/MTSSummarizerBackend/api/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from django.utils import timezone
+from .models import Article
+
+
+class ArticleDataBaseSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Article
+        fields = ('url', 'title', 'summary', 'created_at')
+        read_only_fields = ('title', 'summary', 'created_at')
+
+    def create(self, validated_data):
+        url = validated_data['url']
+        user = validated_data['user']
+        title = url.split('/')[-2]
+        summary = f'{title} - {url}'
+        return Article.objects.create(url=url, title=title,
+                                      summary=summary, user=user)
+
+
+class ArticleLiteSerialiser(serializers.Serializer):
+    url = serializers.URLField()
+    title = serializers.CharField(required=False)
+    summary = serializers.CharField(required=False)
+    created_at = serializers.DateTimeField(required=False)
+
+    def create(self, validated_data):
+        url = validated_data['url']
+        title = url.split('/')[-2]
+        summary = f'{title} - {url}'
+        created_at = timezone.now()
+        obj = {'url': url,
+               'title': title,
+               'summary': summary,
+               'created_at': created_at}
+        return obj
+
+
+class ArticleListSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Article
+        fields = ('id', 'title', 'created_at')
+        read_only_feilds = ('id', 'title', 'created_at')
